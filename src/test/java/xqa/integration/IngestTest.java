d9b63c0913e8688ed07b58a47f51c752e5b1c6fd
James Sears <james.hn.sears@gmail.com>
2018-12-26 14:28:32 +0000

    move to junit-docker-http

--- src/test/java/xqa/integration/IngestTest.java
+++ src/test/java/xqa/integration/IngestTest.java
@@ -3,18 +3,34 @@ package xqa.integration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.github.jameshnsears.configuration.ConfigurationAccessor;
+import com.github.jameshnsears.configuration.ConfigurationParameterResolver;
+import com.github.jameshnsears.docker.DockerClient;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import xqa.ingest.Ingest;
 
+@ExtendWith(ConfigurationParameterResolver.class)
 class IngestTest {
     private String getResource() {
         return Thread.currentThread().getContextClassLoader().getResource("test-data").getPath();
     }
 
     @Test
-    void ingestMain() throws Exception {
-        assertEquals(2, Ingest.executeIngest((new String[]{"-message_broker_host", "127.0.0.1", "-path", getResource()})));
+    void ingestMain(final ConfigurationAccessor configurationAccessor) throws Exception {
+        DockerClient dockerClient = new DockerClient();
+
+        try {
+            dockerClient.pull(configurationAccessor.images());
+            dockerClient.startContainers(configurationAccessor);
+
+            assertEquals(
+                    2,
+                    Ingest.executeIngest((new String[]{"-message_broker_host", "127.0.0.1", "-path", getResource()})));
+        } finally {
+            dockerClient.rmContainers(configurationAccessor);
+        }
     }
 
     @Test
